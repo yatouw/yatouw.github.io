d001e8869703e40bdaec4d69a66f8fd0be32e6ad
Yatouwu <154877343+Yatouwu@users.noreply.github.com>
2024-10-05 19:42:14 +0800

    fix build error

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,15 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({ pages: "build", assets: "build", fallback: undefined, precompress: false, strict: true }),
+		
+		prerender: {
+			handleHttpError: ({ path, status }) => {
+				// Log or handle 404 errors gracefully during the build
+				if (status === 404) {
+					console.warn(`Prerendering failed: ${status} for ${path}`);
+				}
+			}
+		}
 	}
 };
 
