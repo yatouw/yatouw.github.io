1509613eefdac3df55fac0d990f445f0ec74b2d6
Yatouwu <154877343+Yatouwu@users.noreply.github.com>
2024-10-09 03:19:38 +0800

    adding picture from home and make it responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,19 @@
     { name: "About Me", href: "about-me/" },
     { name: "Contact", href: "contact/" },
   ];
+
+  let isMenuOpen = false;
+
+  function toggleMenu() {
+    isMenuOpen = !isMenuOpen;
+  }
 </script>
 
 <nav class="nav-bar">
   <div class="container mx-auto flex justify-between items-center">
     <a href="/" class="brand-title">MyPortfolio</a>
+    
+    <!-- Desktop -->
     <div class="space-x-6 hidden md:flex">
       {#each navItems as item}
         <a href={item.href} class="nav-link">
@@ -18,13 +26,27 @@
         </a>
       {/each}
     </div>
+    
+    <!-- Mobile Toggle Button -->
     <div class="md:hidden">
-      <button class="menu-toggle" id="menu-toggle">
+      <button class="menu-toggle" on:click={toggleMenu}>
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"/>
         </svg>
       </button>
     </div>
   </div>
+
+  <!-- Mobile  -->
+  {#if isMenuOpen}
+    <div class="menu-items md:hidden">
+      {#each navItems as item}
+        <a href={item.href} class="nav-link block py-2 px-4">
+          {item.name}
+        </a>
+      {/each}
+    </div>
+  {/if}
 </nav>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
+<script>
+	import dp from '$lib/assets/dp.jpg';
+</script>
+
 <section class="min-h-screen hero-section">
   <div class="text-right text-white max-w-lg">
     <h1 class="hero-title">Hi, I'm Jayvee</h1>
     <p class="hero-description">
       I'm a BSIT student, focused on building functional websites.
     </p>
+  </div>
+  <div class="hero-image-container">
+    <img src={dp} alt="Jayvee" class="hero-image">
   </div>
 </section>
